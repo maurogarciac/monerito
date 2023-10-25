1341b01cdbd70d953686ef7ef937e6cfabec8397
maurogarciac <maurogarciacoto@gmail.com>
2023-10-25 00:49:10 -0300

    back to the drawing board

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 #[macro_use] extern crate rocket;
 
 use std::io::Read;
+use std::task::Poll::{Pending, Ready};
+use rocket::tokio::task;
+use rocket::tokio::task::JoinHandle;
 use value_api_reader::process_values;
 
 mod value_api_reader;
 mod model;
 
 #[get("/")]
-async fn index() -> String {
-    process_values().await.to_string()
+fn index() -> &'static str {
+    "yeah this doesn't really work the way I expected it to work"
 }
 
 
